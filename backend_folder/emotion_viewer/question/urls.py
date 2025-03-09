e7629768af216b7dd71d830e1a2cdb251b6649e3
Jai Desar <141903653+Ghostblaster08@users.noreply.github.com>
2025-03-09 13:04:35 +0530

    djangoo bug fixed

--- backend_folder/emotion_viewer/question/urls.py
+++ backend_folder/emotion_viewer/question/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import process_resume  # Import the updated view function
+from .views import upload_resume # Import the updated view function
 
 urlpatterns = [
-    path('upload-resume/', process_resume, name='upload_resume'),  # Updated API endpoint for processing resumes
+    path('upload-resume/', upload_resume, name='upload_resume'),  # Updated API endpoint for processing resumes
 ]
